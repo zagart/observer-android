d2609bd4ad387748ab5948d2f945bc0de18c21e6
zagart <zagart.dev@mail.ru>
2016-09-27 00:10:48 +0300

    Commit for rebase #1.

--- app/src/main/java/by/grodno/zagart/observer/observerandroid/MainActivity.java
+++ app/src/main/java/by/grodno/zagart/observer/observerandroid/MainActivity.java
@@ -3,6 +3,9 @@ package by.grodno.zagart.observer.observerandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Commit text #1 for rebase.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
