77d3d19dea2a7f66cb1ee66d2a263efbf0c77d27
zagart <zagart.dev@mail.ru>
2016-10-05 15:08:01 +0300

    Approximate structure of activities has been formed. Initial commit

--- app/src/main/java/by/grodno/zagart/observer/observerandroid/MusicActivity.java
+++ app/src/main/java/by/grodno/zagart/observer/observerandroid/MusicActivity.java
@@ -4,24 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-
-
 /**
- * Main activity.
+ * Static music activity.
  */
-public class MainActivity extends AppCompatActivity {
+public class MusicActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        (getSupportActionBar()).hide();
+        setContentView(R.layout.music_layout);
     }
 
-    public void openMusicActivity(View view) {
-        Intent intent = new Intent(this, MusicActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        startActivity(intent);
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
     }
 
 }
