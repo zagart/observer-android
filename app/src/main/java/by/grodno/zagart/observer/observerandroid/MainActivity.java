a47ba6222537c7bd755f369e6464e2ea0aeaa712
zagart <zagart.dev@mail.ru>
2016-10-06 21:19:03 +0300

    Branch has been cleaned up. Shared preferences initial commit

--- app/src/main/java/by/grodno/zagart/observer/observerandroid/MainActivity.java
+++ app/src/main/java/by/grodno/zagart/observer/observerandroid/MainActivity.java
@@ -1,10 +1,8 @@
 package by.grodno.zagart.observer.observerandroid;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 
 /**
@@ -15,13 +13,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void openMusicActivity(View view) {
-        Intent intent = new Intent(this, MusicActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        startActivity(intent);
     }
 
 }
