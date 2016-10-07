f9896d4ae1003b6560f4ca7431c86b8d879a7139
zagart <zagart.dev@mail.ru>
2016-10-07 22:36:23 +0300

    Unnesesary code cleaned out

--- app/src/main/java/by/grodno/zagart/observer/observerandroid/activities/A3.java
+++ app/src/main/java/by/grodno/zagart/observer/observerandroid/activities/A3.java
@@ -1,6 +1,5 @@
 package by.grodno.zagart.observer.observerandroid.activities;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
@@ -8,7 +7,6 @@ import by.grodno.zagart.observer.observerandroid.MainActivity;
 
 import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK;
 import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
-import static by.grodno.zagart.observer.observerandroid.utils.SharedPreferencesUtil.PREF_FILE;
 
 /**
  * Activity #3.
@@ -23,12 +21,7 @@ public class A3 extends A {
 
     @Override
     protected void openNextActivity(int flags) {
-        SharedPreferences replies = getSharedPreferences(PREF_FILE, MODE_PRIVATE);
-        SharedPreferences.Editor editor = replies.edit();
-        editor.clear();
-        editor.apply();
         mActivityReply = null;
         super.openNextActivity(FLAG_ACTIVITY_CLEAR_TASK | FLAG_ACTIVITY_NEW_TASK);
     }
-
 }
