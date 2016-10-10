485c7a152000c44501002032935be424d7df674d
zagart <zagart.dev@mail.ru>
2016-10-11 02:38:57 +0300

    Several styles added to main layout

--- app/src/main/java/by/grodno/zagart/observer/observerandroid/MainActivity.java
+++ app/src/main/java/by/grodno/zagart/observer/observerandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package by.grodno.zagart.observer.observerandroid;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.hide();
+        }
     }
 }
